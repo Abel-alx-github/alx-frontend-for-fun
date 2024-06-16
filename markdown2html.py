356e602e30cad88a15391ff4bc106b2c796d542a
abel-alx-github <abeltibeso323@gmail.com>
2024-06-17 00:43:25 +0300

    markdown to html converting script

--- markdown2html.py
+++ markdown2html.py
@@ -58,15 +58,15 @@ def main():
   """
 
   if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    sys.exit(1)
+    print("Usage: ./markdown2html.py README.md README.html")
+    exit(1)
 
   markdown_file = sys.argv[1]
   to_html = sys.argv[2]
 
   if not os.path.exists(markdown_file):
-    print(f"Missing {markdown_file}", file=sys.stderr)
-    sys.exit(1)
+    print(f"Missing {markdown_file}")
+    exit(1)
 
   with open(markdown_file, 'r') as md_f, open(to_html, 'w') as html_f:
     current_paragraph = []
